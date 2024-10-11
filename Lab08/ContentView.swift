972633ee9df10d523f5c3aeafef38e61f90b81a8
mitrajesse <mitrajesse@gmail.com>
2024-10-11 14:33:48 -0400

    Initial Commit

---
+++ Lab08/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab08
+//
+//  Created by Jesse Mitra on 10/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab08/Lab08App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab08App.swift
+//  Lab08
+//
+//  Created by Jesse Mitra on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab08App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
