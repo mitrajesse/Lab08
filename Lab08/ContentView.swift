28afd95360181a682bc40820f79e7b4e2ed07e29
mitrajesse <mitrajesse@gmail.com>
2024-10-17 20:58:21 -0400

    init commit

--- Lab08/ContentView.swift
+++ Lab08/ContentView.swift
@@ -4,18 +4,21 @@
 //
 //  Created by Jesse Mitra on 10/11/24.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Chair(legWidth: 20, legHeight: 150, seatLength: 200, seatThickness: 100)
+                .fill(Color.black)
+                .padding([.leading, .bottom], 90.0)
+
+            Spacer()
+            
+            Chair(legWidth: 10, legHeight: 75, seatLength: 100, seatThickness: 50)
+                .fill(Color.black)
+                .padding()
         }
-        .padding()
     }
 }
 
